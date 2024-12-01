e4ba71575a829eb479fbe4a7b6c28431e249c720
ivanHuerta14 <146044360+ivanHuerta14@users.noreply.github.com>
2024-12-01 03:30:54 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,22 @@
 <?php
 header('Content-Type: application/json'); // Asegura que el contenido siempre sea JSON
 
-$servername = "localhost";
-$username = "root"; 
-$password = ""; 
-$dbname = "musica_db";
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
+$port = getenv('DB_PORT');
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Establecer conexión con PDO incluyendo el puerto
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Mensaje de éxito
+   //echo json_encode(['success' => 'Conexión exitosa a la base de datos']);
 } catch (PDOException $e) {
     // En caso de error, devolver JSON con mensaje de error
-    echo json_encode(['error' => 'Error de conexión: ' . $e->getMessage()]);
+   // echo json_encode(['error' => 'Error de conexión: ' . $e->getMessage()]);
     exit(); // Termina la ejecución si falla la conexión
 }
 ?>
